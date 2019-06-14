40865c902ac762683c58174b8c8e8ed1fe12b2c4
mxc <matt@curinga.com>
2019-06-14 14:39:11 -0400

    merged and running

--- src/index.js
+++ src/index.js
@@ -3,11 +3,5 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-=======
-import './css/main.scss';
-
-// @import '/css/index.scss';
->>>>>>> f343ae45882f66b7db9c1fe8eb6f0dac3dd2632e
 
 ReactDOM.render(<App />, document.getElementById('root'));
